b1483c3364fcd1d9e9b34f7000560ab07c3d6233
23fulgence23 <fulgencedamivelo6@gmail.com>
2026-09-14 08:59:07 +0200

    fix: CORS pour le nouveau domaine iraky-delivery.vercel.app

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -4,9 +4,9 @@ return [
     'allowed_methods'          => ['*'],
     'allowed_origins'          => [
         'http://localhost:3000',
-        'https://projet-iraky-delivery.vercel.app',
+        'https://iraky-delivery.vercel.app',
     ],
-    'allowed_origins_patterns' => ['#^https://projet-iraky-delivery.*\.vercel\.app$#'],
+    'allowed_origins_patterns' => ['#^https://iraky-delivery.*\.vercel\.app$#'],
     'allowed_headers'          => ['*'],
     'exposed_headers'          => [],
     'max_age'                  => 86400,
